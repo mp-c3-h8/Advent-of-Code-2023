e5bc851eda8c75ca563b2be0aec8a77c683cf2da
mp <no@e.mail>
2025-12-16 09:20:04 +0100

    day11

---
+++ day11/p2.py
@@ -0,0 +1,35 @@
+import os.path
+from collections import defaultdict
+from itertools import combinations
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input_path = os.path.join(dir_path, "input.txt")
+
+data = open(input_path).read().splitlines()
+dimy, dimx = len(data), len(data[0])
+
+hor_expansion = 0
+planets_by_x: defaultdict[int, list[int]] = defaultdict(list)
+empty_columns = [1] * dimx
+for y, row in enumerate(data):
+    is_empty = True
+    for x, c in enumerate(list(row)):
+        if c == '#':
+            is_empty = False
+            empty_columns[x] = 0
+            planets_by_x[x].append(y+hor_expansion)
+    if is_empty:
+        hor_expansion += 999_999
+
+planets: list[tuple[int, int]] = []
+ver_expansion = 0
+for x in range(dimx):
+    ver_expansion += empty_columns[x] * 999_999
+    for y in planets_by_x[x]:
+        planets.append((y, x+ver_expansion))
+
+ans = 0
+for (y1, x1), (y2, x2) in combinations(planets, 2):
+    ans += abs(y2-y1) + abs(x2-x1)
+
+print("Part 2:", ans)
