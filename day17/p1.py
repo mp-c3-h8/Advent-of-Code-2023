cbe53f6d6997080f2ff211bf6361f46d9a2418ad
mp <no@e.mail>
2025-12-19 09:45:19 +0100

    day17

---
+++ day17/p1.py
@@ -0,0 +1,70 @@
+import os.path
+from heapq import heapify, heappush, heappop
+from dataclasses import dataclass, field
+
+type Position = complex
+type Direction = complex
+type Node = tuple[Position, Direction, int]  # int: number of straight steps
+
+
+@dataclass(order=True)
+class PriorityNode:
+    cost: int  # priority
+    node: Node = field(compare=False)
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input_path = os.path.join(dir_path, "input.txt")
+
+data = open(input_path).read().splitlines()
+grid = {complex(i, j): int(c) for j, row in enumerate(data) for i, c in enumerate(row)}
+dimy, dimx = len(data), len(data[0])
+
+
+def valid_neighbors(node: Node) -> list[Node]:
+    pos, d, s = node
+    res = []
+    if (left := pos + d*1j) in grid:
+        res.append((left, d*1j, 1))
+    if (right := pos - d*1j) in grid:
+        res.append((right, -d*1j, 1))
+    if (straight := pos + d) in grid and s < 3:
+        res.append((straight, d, s+1))
+    return res
+
+
+# modified dijkstra
+def dijkstra(source: Position, dest: Position) -> int:
+    shortest_paths: dict[Node, int] = {}
+    done: set[Node] = set()
+
+    # init q
+    q: list[PriorityNode] = [PriorityNode(0, (source, 1, 0)), PriorityNode(0, (source, 1j, 0))]
+    heapify(q)
+
+    while q:
+        pnode = heappop(q)
+        cost, node = pnode.cost, pnode.node
+
+        if node[0] == dest:
+            return cost
+
+        if node in done:
+            continue
+        done.add(node)
+
+        for new_node in valid_neighbors(node):  # update valid neighbor
+            new_pos = new_node[0]
+            new_cost = cost + grid[new_pos]
+            if new_node in shortest_paths and shortest_paths[new_node] <= new_cost:
+                continue
+            shortest_paths[new_node] = new_cost
+            heappush(q, PriorityNode(new_cost, new_node))
+
+    return -1
+
+
+source = complex(0, 0)
+dest = complex(dimx-1, dimy-1)
+ans = dijkstra(source, dest)
+print('Part 1:', ans)
